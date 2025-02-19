85f791745fded5500b6c627a17efeb63f51777b9
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-02-19 13:23:42 +0900

    feat(COMPONET): CGRAPH - PROPS VAR & CHILD

--- daoxin/src/components/CGraph.tsx
+++ daoxin/src/components/CGraph.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './CGraph.css';
 
-export interface CGraphProps {}
+export interface CGraphProps {
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
 
-const CGraph: React.FC<CGraphProps> = () => {
+const CGraph: React.FC<CGraphProps> = ({ style, children }) => {
   return (
-    <div className='c-graph'>
-      <div className='inner-area'></div>
+    <div
+      className='c-graph'
+      style={{
+        ...style,
+      }}>
+      <div className='inner-area'>{children}</div>
     </div>
   );
 };
