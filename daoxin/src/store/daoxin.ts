9ef82a444b58ef58d25eaa61231e727fabcd4e5b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-03-07 23:19:12 +0900

    feat(STORE): DAOXIN STATE STRUCT & DAILY LIST

--- daoxin/src/store/daoxin.ts
+++ daoxin/src/store/daoxin.ts
@@ -1,9 +1,35 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
-export const Daoxin = atom({
+export interface DaoXinProps {
+  list: Array<DaoXinListProps>;
+  gauge: number;
+  updateAt: string;
+}
+
+export type DaoXinListProps = {
+  idx: number;
+  todo: string;
+  completed: boolean;
+  updateAt: string;
+};
+
+export const DaoXin = atom<DaoXinProps>({
   key: 'daoxin',
   default: {
     gauge: 1,
-    list: {},
+    list: [],
+    updateAt: '',
+  },
+});
+
+export const DailyList = selector({
+  key: 'dailylist',
+  get: ({ get }) => {
+    return get(DaoXin).list;
+  },
+  set: ({ get, set }, value) => {
+    const prev = get(DaoXin);
+    prev.list = value as Array<DaoXinListProps>;
+    set(DaoXin, prev);
   },
 });
