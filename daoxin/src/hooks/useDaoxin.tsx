fbadc0687127ae68a792728e8984de8fae65fcd8
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-03-14 09:11:48 +0900

    feat(HOOK): DAOXIN - LOCALSTORAGE CRYPT

--- daoxin/src/hooks/useDaoxin.tsx
+++ daoxin/src/hooks/useDaoxin.tsx
@@ -1,36 +1,69 @@
-import { useRecoilState } from 'recoil';
+import { useAtom } from 'jotai';
+import { decryptData, encryptData, encryptedDataProps } from '../utils/crypt';
 
 import { DaoXin, DailyList, DaoXinProps } from '../store/daoxin';
 
 const DAOXIN = 'daoxin';
-const SALT = '일체유심조'; // 로컬스토리지에 저장되는 데이터를 간단히 암호화해서 관리하자
+const SALT = '일체유심조';
 
 const useDaoxin = () => {
-  const [dao, setDao] = useRecoilState(DaoXin);
-  const [dList, setDList] = useRecoilState(DailyList);
+  const [dao, setDao] = useAtom(DaoXin);
+  const [dList, setDList] = useAtom(DailyList);
 
-  const initDaoxin = () => {
+  const initDaoxin = async () => {
     const state = localStorage.getItem(DAOXIN);
     if (state === null) {
       const defaultValue: DaoXinProps = {
         gauge: 1,
-        list: [],
-        updateAt: Date.toString(),
+        list: [
+          {
+            idx: 0,
+            todo: '참장공',
+            completed: false,
+            updateAt: new Date().toLocaleDateString(),
+          },
+          {
+            idx: 1,
+            todo: '명상',
+            completed: false,
+            updateAt: new Date().toLocaleDateString(),
+          },
+          {
+            idx: 2,
+            todo: '운동',
+            completed: false,
+            updateAt: new Date().toLocaleDateString(),
+          },
+        ],
+        updateAt: new Date().toLocaleDateString(),
       };
-      localStorage.setItem(DAOXIN, JSON.stringify(defaultValue));
+      localStorage.setItem(
+        DAOXIN,
+        JSON.stringify(
+          await encryptData(JSON.stringify(defaultValue), SALT, SALT),
+        ),
+      );
       setDao(defaultValue);
     } else {
-      const prevValue = JSON.parse(state) as DaoXinProps;
+      const encrypt = JSON.parse(state) as encryptedDataProps;
+      const prevState = await decryptData(
+        encrypt.encryptedData,
+        encrypt.iv,
+        SALT,
+        SALT,
+      );
       // 시간이 지난 것에 따라서 updateAt를 갱신하고 수행하지 못한 과제가 있을시 하루당 1씩 gauge를 감소시킨다.
-      setDao(prevValue);
+      setDao(JSON.parse(prevState));
     }
   };
 
   const checkList = (idx: number) => {
     // completed를 reduce로 받아서 모든 목록이 끝나면 gauge를 1증가시킨다.
     // list를 중간에 추가할시 관련해서 처리할 로직은 조금 고민해봐야함.
-    setDList((prev) =>
-      prev.map((i, _i) => (idx === _i ? { ...i, completed: true } : i)),
+    setDList((prev: any[]) =>
+      prev.map((i: any, _i: number) =>
+        idx === _i ? { ...i, completed: true } : i,
+      ),
     );
   };
 
