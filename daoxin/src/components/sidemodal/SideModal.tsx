3b619a97bcba6d380878a7532bc5990bd745d6a8
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-03-24 12:01:55 +0900

    feat(COMPONENT): SIDE MODAL - INIT

---
+++ daoxin/src/components/sidemodal/SideModal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './SideModal.css';
+
+export interface SideModalProps {
+  children?: React.ReactNode;
+  closeCb?: VoidFunction;
+}
+
+const SideModal: React.FC<SideModalProps> = ({ children, closeCb }) => {
+  return (
+    <div className='side-modal'>
+      <article className='side-content'>{children}</article>
+      <button onClick={closeCb}>Close</button>
+    </div>
+  );
+};
+
+export default SideModal;
