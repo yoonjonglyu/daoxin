c91ba719c109a16c14227b6d729d40e90e92292b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-09-28 02:42:16 +0900

    chore: VITE BUILD OPTION RM

--- daoxin/vite.config.ts
+++ daoxin/vite.config.ts
@@ -38,21 +38,5 @@ export default defineConfig({
         ],
       },
     }),
-  ],
-  build: {
-    outDir: 'dist',
-    manifest: 'assets/manifest.json',
-    rollupOptions: {
-      output: {
-        // ✅ 해시 제거: 이미지 등 asset에 대해 해시 없이 고정 이름으로 출력
-        assetFileNames: (info) => {
-          // 이미지인 경우만 hash 제거
-          if (/\.(png|jpg|jpeg|svg|ico)$/i.test(info.name ?? '')) {
-            return 'assets/[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]'; // 다른 건 기본값 유지
-        },
-      },
-    },
-  },
+  ]
 });
