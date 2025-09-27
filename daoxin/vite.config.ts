112801912fe75652664843219217b589080a34ca
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-09-28 02:02:52 +0900

    chore: ADD PWA

--- daoxin/vite.config.ts
+++ daoxin/vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 
 export default defineConfig({
@@ -14,6 +15,27 @@ export default defineConfig({
         appDescription: 'Daoxin - A simple and elegant habit app',
         developerName: 'ISA',
         path: 'assets/',
+      },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        id: 'daoxin',
+        scope: '/daoxin/',
+        name: 'Daoxin',
+        short_name: 'daoxin',
+        start_url: '/daoxin/',
+        display: 'standalone',
+        description: 'Daoxin - A simple and elegant habit app',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          { src: 'pwa-192x192.png', type: 'image/png', sizes: '192x192' },
+          { src: 'pwa-512x512.png', type: 'image/png', sizes: '512x512' },
+        ],
       },
     }),
   ],
