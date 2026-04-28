124bc9d113d0160f61470247b3159e07469c895c
Isa(yunjongryu) <yunjonglyu@gmail.com>
2026-04-28 21:32:47 +0900

    chore: CAPACITOR

---
+++ daoxin/android/app/src/main/java/com/yoonjongryu/daoxin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yoonjongryu.daoxin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ daoxin/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yoonjongryu.daoxin',
+  appName: 'ABILITY: DaoXin(道心)',
+  webDir: 'dist'
+};
+
+export default config;
