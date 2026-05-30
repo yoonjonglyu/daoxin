0bb91d507808e2c1154d17ee5f7f9e99ac3edce1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2026-05-31 06:50:21 +0900

    Updates

--- assets/index-j7QUJTUs.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D0FO6cFR.js","assets/index-B6gteH_n.js","assets/index-DHohpu3X.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-B6gteH_n.js";const _=p("App",{web:()=>r(()=>import("./web-D0FO6cFR.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
---
+++ assets/web-b6HhTmap.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-C_BLdivY.js";class n extends e{async canShare(){return typeof navigator>"u"||!navigator.share?{value:!1}:{value:!0}}async share(a){if(typeof navigator>"u"||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}export{n as ShareWeb};
